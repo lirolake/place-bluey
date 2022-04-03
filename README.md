0d34fc63355946c922a1b893b40b5a9868fe0e6c
Liam <lirolake@gmail.com>
2022-04-03 19:09:08 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# PlaceBluey Bot
+
+### Installation instructions
+
+before you start, make sure your cooldown has run out!
+
+1. Install the [Tampermonkey](https://www.tampermonkey.net/) browser extension.
+2. Click on this link: [https://github.com/lirolake/place-bluey/raw/master/placeblueybot.user.js](https://github.com/lirolake/place-bluey/raw/master/placeblueybot.user.js). If everything went well you'll see Tampermonkey ask you to add it. Click **Install**.
+3. Reload your **r/place** tab. If everything went well, you'll see "Get Access Token..." in the top right of your screen. The bot is now active, You'll be able to see what the bot is doing through these messages.
+
+### Cons of this bot
+
+- When the bot places a pixel, it will look as if it wasn't placed, while the bot has already done that (and thus you're in cooldown). You can see the cooldown in the top right of your screen.
+
+## Headless bot
+
+### You can get an access token
+1. Go to [r/place](https://www.reddit.com/r/place/)
+2. Open the browser console (F12 -> Click on console)
+3. paste this code and press enter:
+```
+async function getAccessToken() {
+	const usingOldReddit = window.location.href.includes('new.reddit.com');
+	const url = usingOldReddit ? 'https://new.reddit.com/r/place/' : 'https://www.reddit.com/r/place/';
+	const response = await fetch(url);
+	const responseText = await response.text();
+
+	return responseText.split('\"accessToken\":\"')[1].split('"')[0];
+}
+
+await getAccessToken()
+```
+4. The text between quotes (`"`) is your access token.
+
+### Installation instructions
+
+1. Install [NodeJS](https://nodejs.org/).
+2. Download the bot via [this link](https://github.com/lirolake/place-bluey/archive/refs/heads/master.zip).
+3. Extract the bot anywhere on your desktop
+4. Open a command prompt/terminal in this folder
+    Windows: Shift+right mouse button in the folder -> Click on "open Powershell here"
+
+    Mac: No clue, sorry!
+
+    Linux: Is this necessary?
+5. install the dependencies: `npm i`
+6. execute the bot `node bot.js ACCESS_TOKEN_HERE`
+7. BONUS: You can repeat these steps for any amount of accounts you'd want. Keep in mind to use different accounts.
---
+++ bot.js
@@ -0,0 +1,312 @@
+import fetch from 'node-fetch';
+import getPixels from "get-pixels";
+import WebSocket from 'ws';
+
+const VERSION_NUMBER = 2;
+
+console.log(`PlaceBluey headless client V${VERSION_NUMBER}`);
+
+const args = process.argv.slice(2);
+
+if (args.length != 1 && !process.env.ACCESS_TOKEN) {
+    console.error("Missing access token.")
+    process.exit(1);
+}
+
+let accessTokens = (process.env.ACCESS_TOKEN || args[0]).split(',');
+let defaultAccessToken = accessTokens[0];
+
+if (accessTokens.length > 4) {
+    console.warn("More than 4 reddit accounts per IP address is not recommended!")
+}
+
+var socket;
+var currentOrders;
+var currentOrderList;
+
+const COLOR_MAPPINGS = {
+	'#BE0039': 1,
+  '#FF4500': 2,
+  '#FFA800': 3,
+  '#FFD635': 4,
+  '#00A368': 6,
+  '#00CC78': 7,
+  '#7EED56': 8,
+  '#00756F': 9,
+  '#009EAA': 10,
+  '#2450A4': 12,
+  '#3690EA': 13,
+  '#51E9F4': 14,
+  '#493AC1': 15,
+  '#6A5CFF': 16,
+  '#811E9F': 18,
+  '#B44AC0': 19,
+  '#FF3881': 22,
+  '#FF99AA': 23,
+  '#6D482F': 24,
+  '#9C6926': 25,
+  '#000000': 27,
+  '#898D90': 29,
+  '#D4D7D9': 30,
+  '#FFFFFF': 31
+};
+
+let rgbaJoin = (a1, a2, rowSize = 1000, cellSize = 4) => {
+    const rawRowSize = rowSize * cellSize;
+    const rows = a1.length / rawRowSize;
+    let result = new Uint8Array(a1.length + a2.length);
+    for (var row = 0; row < rows; row++) {
+        result.set(a1.slice(rawRowSize * row, rawRowSize * (row+1)), rawRowSize * 2 * row);
+        result.set(a2.slice(rawRowSize * row, rawRowSize * (row+1)), rawRowSize * (2 * row + 1));
+    }
+    return result;
+};
+
+let getRealWork = rgbaOrder => {
+    let order = [];
+    for (var i = 0; i < 2000000; i++) {
+        if (rgbaOrder[(i * 4) + 3] !== 0) {
+            order.push(i);
+        }
+    }
+    return order;
+};
+
+let getPendingWork = (work, rgbaOrder, rgbaCanvas) => {
+    let pendingWork = [];
+    for (const i of work) {
+        if (rgbaOrderToHex(i, rgbaOrder) !== rgbaOrderToHex(i, rgbaCanvas)) {
+            pendingWork.push(i);
+        }
+    }
+    return pendingWork;
+};
+
+(async function () {
+	connectSocket();
+
+    // Try to stagger pixel placement
+    const interval = 300 / accessTokens.length;
+    var delay = 0;
+    for (const accessToken of accessTokens) {
+        setTimeout(() => attemptPlace(accessToken), delay * 1000);
+        delay += interval;
+    }
+
+    setInterval(() => {
+        if (socket) socket.send(JSON.stringify({ type: 'ping' }));
+    }, 5000);
+})();
+
+function connectSocket() {
+    console.log('Connecting to PlaceBluey server...')
+
+    socket = new WebSocket('ws://place-bluey.ddns.net:3987/api/ws');
+
+    socket.onerror = function(e) {
+        console.error("Socket error: " + e.message)
+    }
+
+    socket.onopen = function () {
+        console.log('Connected to PlaceBluey server!')
+        socket.send(JSON.stringify({ type: 'getmap' }));
+        socket.send(JSON.stringify({ type: 'brand', brand: `nodeheadlessV${VERSION_NUMBER}` }));
+    };
+
+    socket.onmessage = async function (message) {
+        var data;
+        try {
+            data = JSON.parse(message.data);
+        } catch (e) {
+            return;
+        }
+
+        switch (data.type.toLowerCase()) {
+            case 'map':
+                console.log(`New folder loaded (Reason: ${data.reason ? data.reason : 'Connected to Server'})`)
+                currentOrders = await getMapFromUrl(`http://place-bluey.ddns.net:3987/maps/${data.data}`);
+                currentOrderList = getRealWork(currentOrders.data);
+                break;
+            default:
+                break;
+        }
+    };
+
+    socket.onclose = function (e) {
+        console.warn(`PlaceBluey Server has disconnected: ${e.reason}`)
+        console.error('Socket Error: ', e.reason);
+        socket.close();
+        setTimeout(connectSocket, 1000);
+    };
+}
+
+async function attemptPlace(accessToken) {
+    let retry = () => attemptPlace(accessToken);
+    if (currentOrderList === undefined) {
+        setTimeout(retry, 2000); // probeer opnieuw in 2sec.
+        return;
+    }
+
+    var map0;
+    var map1;
+    try {
+        map0 = await getMapFromUrl(await getCurrentImageUrl('0'))
+        map1 = await getMapFromUrl(await getCurrentImageUrl('1'));
+    } catch (e) {
+        console.warn('Error retrieving folder: ', e);
+        setTimeout(retry, 15000); // probeer opnieuw in 15sec.
+        return;
+    }
+
+    const rgbaOrder = currentOrders.data;
+    const rgbaCanvas = rgbaJoin(map0.data, map1.data);
+    const work = getPendingWork(currentOrderList, rgbaOrder, rgbaCanvas);
+
+    if (work.length === 0) {
+        console.log(`All pixels are already in the right place! Try again in 30 seconds...`);
+        setTimeout(retry, 30000); // probeer opnieuw in 30sec.
+        return;
+    }
+
+    const percentComplete = 100 - Math.ceil(work.length * 100 / currentOrderList.length);
+    const workRemaining = work.length;
+    const idx = Math.floor(Math.random() * work.length);
+    const i = work[idx];
+    const x = i % 2000;
+    const y = Math.floor(i / 2000);
+    const hex = rgbaOrderToHex(i, rgbaOrder);
+
+    console.log(`Trying to post pixel on ${x}, ${y}... (${percentComplete}% complete, ${workRemaining} remaining)`);
+
+    const res = await place(x, y, COLOR_MAPPINGS[hex], accessToken);
+    const data = await res.json();
+    try {
+        if (data.errors) {
+            const error = data.errors[0];
+            const nextPixel = error.extensions.nextAvailablePixelTs + 3000;
+            const nextPixelDate = new Date(nextPixel);
+            const delay = nextPixelDate.getTime() - Date.now();
+            console.log(`Pixel posted too soon! Next pixel will be placed at ${nextPixelDate.toLocaleTimeString()}.`)
+            setTimeout(retry, delay);
+        } else {
+            const nextPixel = data.data.act.data[0].data.nextAvailablePixelTimestamp + 3000;
+            const nextPixelDate = new Date(nextPixel);
+            const delay = nextPixelDate.getTime() - Date.now();
+            console.log(`Pixel posted on ${x}, ${y}! Next pixel will be placed at ${nextPixelDate.toLocaleTimeString()}.`)
+            setTimeout(retry, delay);
+        }
+    } catch (e) {
+        console.warn('Analyze response error', e);
+        setTimeout(retry, 10000);
+    }
+}
+
+function place(x, y, color, accessToken = defaultAccessToken) {
+    socket.send(JSON.stringify({ type: 'placepixel', x, y, color }));
+    console.log("Placing pixel at (" + x + ", " + y + ") with color: " + color)
+	return fetch('https://gql-realtime-2.reddit.com/query', {
+		method: 'POST',
+		body: JSON.stringify({
+			'operationName': 'setPixel',
+			'variables': {
+				'input': {
+					'actionName': 'r/replace:set_pixel',
+					'PixelMessageData': {
+						'coordinate': {
+							'x': x % 1000,
+							'y': y % 1000
+						},
+						'colorIndex': color,
+						'canvasIndex': (x > 999 ? 1 : 0)
+					}
+				}
+			},
+			'query': 'mutation setPixel($input: ActInput!) {\n  act(input: $input) {\n    data {\n      ... on BasicMessage {\n        id\n        data {\n          ... on GetUserCooldownResponseMessageData {\n            nextAvailablePixelTimestamp\n            __typename\n          }\n          ... on SetPixelResponseMessageData {\n            timestamp\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n'
+		}),
+		headers: {
+			'origin': 'https://hot-potato.reddit.com',
+			'referer': 'https://hot-potato.reddit.com/',
+			'apollographql-client-name': 'mona-lisa',
+			'Authorization': `Bearer ${accessToken}`,
+			'Content-Type': 'application/json'
+		}
+	});
+}
+
+async function getCurrentImageUrl(id = '0') {
+	return new Promise((resolve, reject) => {
+		const ws = new WebSocket('wss://gql-realtime-2.reddit.com/query', 'graphql-ws', {
+        headers : {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0",
+            "Origin": "https://hot-potato.reddit.com"
+        }
+      });
+
+		ws.onopen = () => {
+			ws.send(JSON.stringify({
+				'type': 'connection_init',
+				'payload': {
+					'Authorization': `Bearer ${defaultAccessToken}`
+				}
+			}));
+
+			ws.send(JSON.stringify({
+				'id': '1',
+				'type': 'start',
+				'payload': {
+					'variables': {
+						'input': {
+							'channel': {
+								'teamOwner': 'AFD2022',
+								'category': 'CANVAS',
+								'tag': id
+							}
+						}
+					},
+					'extensions': {},
+					'operationName': 'replace',
+					'query': 'subscription replace($input: SubscribeInput!) {\n  subscribe(input: $input) {\n    id\n    ... on BasicMessage {\n      data {\n        __typename\n        ... on FullFrameMessageData {\n          __typename\n          name\n          timestamp\n        }\n      }\n      __typename\n    }\n    __typename\n  }\n}'
+				}
+			}));
+		};
+
+		ws.onmessage = (message) => {
+			const { data } = message;
+			const parsed = JSON.parse(data);
+
+            if (parsed.type === 'connection_error') {
+                console.error(`[!!] Could not load r/place map: ${parsed.payload.message}. Is the access token still valid?`);
+            }
+
+			// TODO: ew
+			if (!parsed.payload || !parsed.payload.data || !parsed.payload.data.subscribe || !parsed.payload.data.subscribe.data) return;
+
+			ws.close();
+			resolve(parsed.payload.data.subscribe.data.name + `?noCache=${Date.now() * Math.random()}`);
+		}
+
+
+		ws.onerror = reject;
+	});
+}
+
+function getMapFromUrl(url) {
+    return new Promise((resolve, reject) => {
+        getPixels(url, function(err, pixels) {
+            if(err) {
+                console.log("Bad image path")
+                reject()
+                return
+            }
+            console.log("got pixels", pixels.shape.slice())
+            resolve(pixels)
+        })
+    });
+}
+
+function rgbToHex(r, g, b) {
+	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase();
+}
+
+let rgbaOrderToHex = (i, rgbaOrder) =>
+    rgbToHex(rgbaOrder[i * 4], rgbaOrder[i * 4 + 1], rgbaOrder[i * 4 + 2]);
