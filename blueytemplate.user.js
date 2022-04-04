c6d6b86268132dc07d81688336099ce2f907de88
Liam <lirolake@gmail.com>
2022-04-04 12:00:05 +0100

    Create blueytemplate.user.js

---
+++ blueytemplate.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         bluey Place overlay
+// @namespace    https://nightc.at/
+// @version      2
+// @description  Bluey Template
+// @author       NameWasAlreadyTaken
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+            (function () {
+                const i = document.createElement("img");
+                i.src = "http://place-bluey.ddns.net:3987/maps/blank.png";
+                i.onload = () => {
+                  i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+                };
+                return i;
+            })())
+    }, false);
+}
